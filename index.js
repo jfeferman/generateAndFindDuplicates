59ba69da0f48ad22d583d865741727ff6185f6de
Julio Feferman <jfeferman@unito.com.br>
2018-10-24 09:18:06 -0300

    Wrap inside IIFE.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 /*
  * Find duplicate numbers in an array with maximum value N, and size N + 1.
@@ -6,25 +6,26 @@
  * If a number is supplied as an argument (e.g. 'node index.js 100' or 'node index.js num=100'), 
  * than the argument is used as the value of N.
  */
+(function() {
+  const { 
+    buildRandomNumberList, 
+    findDuplicates
+  } = require('./generateAndFindDuplicates')
 
-const { 
-  buildRandomNumberList, 
-  findDuplicates
-} = require('./generateAndFindDuplicates')
+  // Default value of N if no number is passed as argument in the command line.
+  const DEFAULT_N = 1000000
 
-// Default value of N if no number is passed as argument in the command line.
-const DEFAULT_N = 1000000
+  // Handle user supplied value if available
+  const args = process.argv.slice(2)
+  const userValue = args[0] ? args[0].match(/\d+(\.\d+)?/)[0] : null
 
-// Handle user supplied value if available
-const args = process.argv.slice(2)
-const userValue = args[0] ? args[0].match(/\d+(\.\d+)?/)[0] : null
+  // Set N to userValue or default
+  const N = parseInt(userValue) || DEFAULT_N
 
-// Set N to userValue or default
-const N = parseInt(userValue) || DEFAULT_N
+  // Generate a random number list
+  const randomNumberList = buildRandomNumberList(N)
 
-// Generate a random number list
-const randomNumberList = buildRandomNumberList(N)
-
-// Run the find duplicates function and print duplicates array to console
-const duplicates = findDuplicates(randomNumberList)
-console.log('Found duplicate values: ', duplicates)
+  // Run the find duplicates function and print duplicates array to console
+  const duplicates = findDuplicates(randomNumberList)
+  console.log('Found duplicate values: ', duplicates)
+}())
